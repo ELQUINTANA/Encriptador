12940e1308d221a04dc157ac6f43277c0a4055f9
Elias Quintana <equintana.dev@gmail.com>
2023-02-08 11:28:11 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,63 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+//`La letra "e" es convertida para "enter"`
+//`La letra "i" es convertida para "imes"`
+//`La letra "a" es convertida para "ai"`
+//`La letra "o" es convertida para "ober"`
+//`La letra "u" es convertida para "ufat"`
+
+//Botón encriptar
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
+
+//Encriptar
+function encriptar(stringEncriptado){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+
+    stringEncriptado = stringEncriptado.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptado.includes(matrizCodigo[i][0])){
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptado;
+}
+
+//Botón desencriptar
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    
+}
+
+//Desencriptar
+function desencriptar(stringDesencriptado){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+
+    stringDesencriptado = stringDesencriptado.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptado.includes(matrizCodigo[i][1])){
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptado;
+}
+
+
+//Funcion copiar
+function copy() {
+    let copyText = document.querySelector(".mensaje");
+    copyText.select();
+    document.execCommand("copy");
+  }
+  
+  document.querySelector(".copiar").addEventListener("click", copy);
+  
